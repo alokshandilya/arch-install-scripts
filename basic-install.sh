c0179c0a7393a71f301c31ab303c4a843bf4bd2c
alokshandilya <alok.shandilya10@gmail.com>
2021-11-23 13:39:30 +0530

    add more packages

--- basic-install.sh
+++ basic-install.sh
@@ -14,10 +14,10 @@ echo "127.0.1.1 alokpc.localdomain alokpc" >> /etc/hosts
 # change password
 echo root:password| chpasswd
 
-pacman -S grub efibootmgr networkmanager network-manager-applet xorg dialog wpa_supplicant mtools dosfstools reflector base-devel avahi xdg-user-dirs xdg-utils gvfs gvfs-smb git gvfs-mtp nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pulseaudio-alsa pavucontrol bash-completion openssh rsync reflector acpi acpi_call-lts tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet xorg kitty firefox picom pcmanfm gnome-keyring polkit-gnome lxappearance numlockx copyq conky feh bpytop ranger dunst arc-gtk-theme arc-icon-theme ttf-jetbrains-mono ttf-cascadia-code noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid ttf-font-awesome ttf-ibm-plex ttf-joypixels ttf-liberation ttf-opensans dialog wpa_supplicant mtools dosfstools reflector base-devel avahi xdg-user-dirs xdg-utils gvfs gvfs-smb gvfs-mtp nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio pulseaudio-alsa pavucontrol bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 pacman -S --noconfirm xf86-video-intel
-pacman -S --noconfirm nvidia-lts nvidia-utils nvidia-settings
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -39,8 +39,8 @@ useradd -m aloks
 
 # change password
 echo aloks:password| chpasswd
-usermod -aG libvirt aloks 
+usermod -aG libvirt,audio,video aloks 
 
-echo "aloks ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+echo "aloks ALL=(ALL) ALL" >> /etc/sudoers.d/aloks
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
