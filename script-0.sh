2d8eac22a5446a2da3045defd3c7e72eabdd7cf9
Alok Shandilya <alok.shandilya10@gmail.com>
2022-05-20 20:30:45 +0530

    Update script-0.sh
    add var/tmp and srv btrfs subvol

--- script-0.sh
+++ script-0.sh
@@ -6,14 +6,18 @@ mkfs.btrfs -f /dev/nvme0n1p2 -L "BTRFS"
 mount /dev/nvme0n1p2 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
+btrfs su cr /mnt/@srv
 btrfs su cr /mnt/@cache
 btrfs su cr /mnt/@log
+btrfs su cr /mnt/@tmp
 umount /mnt
 mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@ /dev/nvme0n1p2 /mnt
-mkdir -p /mnt/{home,boot/efi,var/cache,var/log}
+mkdir -p /mnt/{home,boot/efi,var/cache,var/log,var/tmp,srv}
 mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
 mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@log /dev/nvme0n1p2 /mnt/var/log
+mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@tmp /dev/nvme0n1p2 /mnt/var/tmp
+mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@srv /dev/nvme0n1p2 /mnt/srv
 mount /dev/nvme0n1p1 /mnt/boot/efi
 
 # Install Arch Linux
