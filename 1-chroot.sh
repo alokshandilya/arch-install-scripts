fc72765355574df4137866d85ac147b5bd7f9c79
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-16 11:14:19 +0530

    change chroot script

--- 1-chroot.sh
+++ 1-chroot.sh
@@ -2,27 +2,18 @@
 
 # Formatting the partitions
 mkfs.fat -F32 /dev/nvme0n1p1 -n "EFI"
-mkfs.btrfs -f /dev/nvme0n1p2 -L "BTRFS"
+mkswap /dev/nvme0n1p2
+mkfs.ext4 /dev/nvme0n1p3 -L "Arch"
+mkfs.ext4 /dev/nvme0n1p4 -L "Home"
 
 # Mount the partitions
-mount /dev/nvme0n1p2 /mnt
-btrfs su cr /mnt/@
-btrfs su cr /mnt/@home
-btrfs su cr /mnt/@srv
-btrfs su cr /mnt/@cache
-btrfs su cr /mnt/@log
-btrfs su cr /mnt/@tmp
-umount /mnt
-mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@ /dev/nvme0n1p2 /mnt
-mkdir -p /mnt/{home,boot/efi,var/cache,var/log,var/tmp,srv}
-mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@home /dev/nvme0n1p2 /mnt/home
-mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@cache /dev/nvme0n1p2 /mnt/var/cache
-mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@log /dev/nvme0n1p2 /mnt/var/log
-mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@tmp /dev/nvme0n1p2 /mnt/var/tmp
-mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@srv /dev/nvme0n1p2 /mnt/srv
+swapon /dev/nvme0n1p2
+mount /dev/nvme0n1p3 /mnt
+mkdir -p /mnt/{home,boot/efi}
 mount /dev/nvme0n1p1 /mnt/boot/efi
+mount /dev/nvme0n1p4 /mnt/home
 
 # pacstrap and chroot
-pacstrap -i /mnt base btrfs-progs linux linux-headers linux-firmware vim nano intel-ucode git
+pacstrap -i /mnt base linux linux-headers linux-firmware vim nano intel-ucode git
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
