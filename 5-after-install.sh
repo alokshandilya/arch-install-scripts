05a710699d1b9b0da14fff2bb351b3b87b58be48
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-16 11:23:11 +0530

    update after-install.sh

--- 5-after-install.sh
+++ 5-after-install.sh
@@ -5,27 +5,26 @@
 ########################################
 if ! command -v paru &> /dev/null
 then
-    printf "\n"
-    printf "########################################\n"
-    printf "##### Installing paru (AUR Helper) #####\n"
-    printf "########################################\n\n"
-    git clone https://aur.archlinux.org/paru-bin.git
-    cd paru-bin
-    makepkg -si
+  printf "\n"
+  printf "########################################\n"
+  printf "##### Installing paru (AUR Helper) #####\n"
+  printf "########################################\n\n"
+  git clone https://aur.archlinux.org/paru-bin.git
+  cd paru-bin
+  makepkg -si
 else
-    printf "\n"
-    printf "#######################################\n"
-    printf "###### paru is already installed ######\n"
-    printf "#######################################\n"
+  printf "\n"
+  printf "#######################################\n"
+  printf "###### paru is already installed ######\n"
+  printf "#######################################\n"
 fi
 
 # Install AUR's
-paru -S timeshift timeshift-autosnap spotify spotify-adblock-git jdk visual-studio-code-bin ananicy-cpp auto-cpufreq android-sdk-platform-tools anydesk-bin brave-bin caffeine-ng github-desktop-bin google-chrome google-earth-pro google-java-format mailspring masterpdfeditor-free optimus-manager optimus-manager-qt picom-git xdman zoom zramd
+paru -S timeshift visual-studio-code-bin ananicy-cpp android-sdk-platform-tools anydesk-bin caffeine-ng google-earth-pro masterpdfeditor-free xdman zoom zramd
 paru -S qogir-icon-theme-git qogir-gtk-theme-git kvantum-theme-qogir-git
 
 sudo systemctl enable --now cronie.service
 sudo systemctl enable --now zramd.service
-sudo systemctl enable --now auto-cpufreq.service
 sudo systemctl enable --now ananicy-cpp.service
 
 # Install Virtualbox and Emacs
@@ -35,26 +34,7 @@ sudo modprobe vboxdrv
 # Install some packages and stuff
 pip install black
 chsh -s /usr/bin/zsh
-paru -S stylua intellij-idea-community-edition pycharm-community-edition gparted mpv yt-dlp libreoffice-fresh tumbler thunar-volman thunar-archive-plugin exa fzf zoxide ripgrep neovim python-neovim xclip libgnome-keyring python-keyring
+paru -S stylua gparted mpv yt-dlp libreoffice-fresh tumbler ffmpegthumbnailer thunar-volman thunar-archive-plugin exa fzf zoxide ripgrep neovim xclip libgnome-keyring python-keyring
 paru -S telegram-desktop redshift flameshot
 curl -fsSL https://fnm.vercel.app/install | bash
 usermod -aG adbusers vboxusers aloks
-
-printf "\n\n#################################\n"
-printf "##### Installing Doom Emacs #####\n"
-printf "#################################\n\n"
-
-mkdir doom-emacs-install-script
-cd doom-emacs-install-script
-git clone https://github.com/alokshandilya/dotfiles.git
-cp -r dotfiles/.config/doom ~/.config/
-cd
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emacs.d/bin/doom install
-cd
-killall emacs
-/usr/bin/emacs --daemon
-
-printf "\n\n#######################################\n"
-printf "##### Doom Emacs script completed #####\n"
-printf "#######################################\n\n"
