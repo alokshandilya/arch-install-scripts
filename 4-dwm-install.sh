d265e3eea92668390d15122352dee0fa410080e3
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-22 00:37:08 +0530

    update dwm-install.sh
    - add required packages
    - remove dmenu [not patched in my repo]
      - [using standard arch package as of now]
    - identation 2 spaces
    - remove libxft-bgra [not required for my build]
      - coz not easily available in debian or fedora.

--- 4-dwm-install.sh
+++ 4-dwm-install.sh
@@ -5,28 +5,28 @@
 ########################################
 if ! command -v paru &> /dev/null
 then
-    printf "\n"
-    printf "########################################\n"
-    printf "##### Installing paru (AUR Helper) #####\n"
-    printf "########################################\n\n"
-    git clone https://aur.archlinux.org/paru-bin.git
-    cd paru-bin
-    makepkg -si
+  printf "\n"
+  printf "########################################\n"
+  printf "##### Installing paru (AUR Helper) #####\n"
+  printf "########################################\n\n"
+  git clone https://aur.archlinux.org/paru-bin.git
+  cd paru-bin
+  makepkg -si
 else
-    printf "\n"
-    printf "#######################################\n"
-    printf "###### paru is already installed ######\n"
-    printf "#######################################\n"
+  printf "\n"
+  printf "#######################################\n"
+  printf "###### paru is already installed ######\n"
+  printf "#######################################\n"
 fi
 
 printf "\n"
 printf "#########################################\n"
 printf "##### Installing suckless utilities #####\n"
 printf "#########################################\n\n"
+# gd is required for my build of st
+paru -S gd
 git clone https://github.com/alokshandilya/suckless.git
-cd suckless/dmenu-5.0
-sudo make clean install
-cd ../dwm
+cd suckless/dwm
 sudo make clean install
 cd ../dwmblocks
 sudo make clean install
@@ -39,7 +39,8 @@ printf "\n"
 printf "#########################################\n"
 printf "##### Installing fonts and packages #####\n"
 printf "#########################################\n\n"
-paru -S libxft-bgra nerd-fonts-jetbrains-mono ttf-fira-code nerd-fonts-fira-code
+#paru -S libxft-bgra 
+paru -S nerd-fonts-jetbrains-mono ttf-fira-code nerd-fonts-fira-code
 paru -S wget bat dash
 
 paru -S lxappearance-gtk3 numlockx copyq conky feh bpytop ranger rofi nitrogen kvantum-qt5 kvantum-theme-materia qt5ct dunst thunar lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan lightdm-gtk-greeter
@@ -58,25 +59,25 @@ printf "##########################################\n\n"
 cd
 DIR="/usr/share/xsessions"
 if [ -d "$DIR" ]; then
-    FILE=/usr/share/xsessions/dwm.desktop
-    if [ -f "$FILE" ]; then
-        printf "$FILE exists\n"
-        sudo rm -rf /usr/share/xsessions/dwm.desktop
-    fi
-    sudo touch /usr/share/xsessions/dwm.desktop
-    echo "[Desktop Entry]" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Encoding=UTF-8" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Name=dwm" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Comment=Dynamic Window Manager" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Exec=~/.dwm/autostart" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  FILE=/usr/share/xsessions/dwm.desktop
+  if [ -f "$FILE" ]; then
+    printf "$FILE exists\n"
+    sudo rm -rf /usr/share/xsessions/dwm.desktop
+  fi
+  sudo touch /usr/share/xsessions/dwm.desktop
+  echo "[Desktop Entry]" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Encoding=UTF-8" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Name=dwm" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Comment=Dynamic Window Manager" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Exec=~/.dwm/autostart" | sudo tee -a /usr/share/xsessions/dwm.desktop
 else
-    sudo mkdir /usr/share/xsessions
-    sudo touch /usr/share/xsessions/dwm.desktop
-    echo "[Desktop Entry]" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Encoding=UTF-8" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Name=dwm" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Comment=Dynamic Window Manager" | sudo tee -a /usr/share/xsessions/dwm.desktop
-    echo "Exec=~/.dwm/autostart" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  sudo mkdir /usr/share/xsessions
+  sudo touch /usr/share/xsessions/dwm.desktop
+  echo "[Desktop Entry]" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Encoding=UTF-8" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Name=dwm" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Comment=Dynamic Window Manager" | sudo tee -a /usr/share/xsessions/dwm.desktop
+  echo "Exec=~/.dwm/autostart" | sudo tee -a /usr/share/xsessions/dwm.desktop
 fi
 
 printf "\n"
