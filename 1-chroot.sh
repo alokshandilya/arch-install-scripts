f45fb5678995c253e0dcde6bac2e8bbbe62a0f23
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-28 17:42:08 +0530

    add encyrption commands

--- 1-chroot.sh
+++ 1-chroot.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Formatting the partitions
+##############################
+##### Without encryption #####
+##############################
+
+#----------------------------------------------------------------------------------------------------------
+
+#####################################
+##### Formatting the partitions #####
+#####################################
 mkfs.fat -F32 /dev/nvme0n1p1 -n "EFI"
 mkfs.btrfs -f /dev/nvme0n1p2 -L "BTRFS"
 
-# Mount the partitions
+################################
+##### Mount the partitions #####
+################################
 mount /dev/nvme0n1p2 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
@@ -23,8 +33,46 @@ mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,
 mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@srv /dev/nvme0n1p2 /mnt/srv
 mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
 mount /dev/nvme0n1p1 /mnt/boot/efi
-
-# pacstrap and chroot
 pacstrap -i /mnt base btrfs-progs linux-zen linux-zen-headers linux-firmware vim intel-ucode git snapper
 genfstab -U /mnt >> /mnt/etc/fstab
 #arch-chroot /mnt
+
+#----------------------------------------------------------------------------------------------------------
+
+###########################
+##### With encryption #####
+###########################
+
+#----------------------------------------------------------------------------------------------------------
+
+#####################################
+##### Formatting the partitions #####
+#####################################
+#mkfs.fat -F32 /dev/nvme0n1p1 -n "EFI"
+#cryptsetup --cipher aes-xts-plain64 --has sha512 --use-random --verify-passphrase luksFormat /dev/nvme0n1p2
+#cryptsetup luksOpen /dev/nvme0n1p2 root
+#mkfs.btrfs -f /dev/mapper/root
+################################
+##### Mount the partitions #####
+################################
+#mount /dev/mapper/root /mnt
+#btrfs su cr /mnt/@
+#btrfs su cr /mnt/@home
+#btrfs su cr /mnt/@srv
+#btrfs su cr /mnt/@cache
+#btrfs su cr /mnt/@log
+#btrfs su cr /mnt/@tmp
+#btrfs su cr /mnt/@snapshots
+#umount /mnt
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@ /dev/mapper/root /mnt
+#mkdir -p /mnt/{home,boot/efi,var/cache,var/log,var/tmp,srv,.snapshots}
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@home /dev/mapper/root /mnt/home
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@cache /dev/mapper/root /mnt/var/cache
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@log /dev/mapper/root /mnt/var/log
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@tmp /dev/mapper/root /mnt/var/tmp
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@srv /dev/mapper/root /mnt/srv
+#mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@snapshots /dev/mapper/root /mnt/.snapshots
+#mount /dev/nvme0n1p1 /mnt/boot/efi
+#pacstrap -i /mnt base btrfs-progs linux-zen linux-zen-headers linux-firmware vim intel-ucode git snapper
+#genfstab -U /mnt >> /mnt/etc/fstab
+##arch-chroot /mnt
