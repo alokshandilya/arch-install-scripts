a415e345b8822824f168989bed13506b8be98076
Alok Shandilya <alok.shandilya10@gmail.com>
2022-07-29 19:29:35 +0530

    improve readability

--- 5-packages-AUR.sh
+++ 5-packages-AUR.sh
@@ -19,14 +19,19 @@ else
   printf "#######################################\n"
 fi
 
-# Install AUR's
-paru -S --needed spotify spotify-adblock-git visual-studio-code-bin ananicy-cpp android-sdk-platform-tools anydesk-bin caffeine-ng google-earth-pro mailspring masterpdfeditor-free xdman zoom zramd envycontrol
+##################################################
+###################### AURs ######################
+##################################################
+## COPY AND RUN ON ANOTHER TERM INSTANCE (TO SAVE TIME) ##
+##
+#paru -S --needed spotify spotify-adblock-git visual-studio-code-bin ananicy-cpp android-sdk-platform-tools anydesk-bin caffeine-ng google-earth-pro mailspring masterpdfeditor-free xdman zoom zramd envycontrol
+#sudo systemctl enable --now cronie.service
+#sudo systemctl enable --now zramd.service
+#sudo systemctl enable --now ananicy-cpp.service
 
-sudo systemctl enable --now cronie.service
-sudo systemctl enable --now zramd.service
-sudo systemctl enable --now ananicy-cpp.service
-
-# Theme [orchis], Icon [Qogir]
+#######################################
+##### Theme[Orchis], Icons[Qogir] #####
+#######################################
 printf "#########################################\n"
 printf "###### Installing Orchis and Qogir ######\n"
 printf "#########################################\n"
@@ -39,17 +44,15 @@ cd Qogir-icon-theme
 sudo ./install.sh
 cd
 
-# Install Virtualbox
-paru -S --needed virtualbox virtualbox-guest-iso virtualbox-ext-oracle
-sudo modprobe vboxdrv
-
-# Install some packages and stuff
-paru -S --needed fish
+#####################################################
+########### VirtualBox and MORE! Packages ###########
+#####################################################
+paru -S --needed virtualbox virtualbox-guest-iso virtualbox-ext-oracle fish
 chsh -s /usr/bin/fish
 paru -S --needed stylua gparted mpv yt-dlp libreoffice-fresh tumbler thunar-volman thunar-archive-plugin exa fzf zoxide ripgrep neovim python-neovim xclip libgnome-keyring python-keyring
-paru -S --needed telegram-desktop redshift flameshot unzip
-# unzip and wget required for fnm
+paru -S --needed wget telegram-desktop redshift flameshot unzip
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+sudo modprobe vboxdrv
 sudo usermod -aG adbusers vboxusers aloks
 
 # Setting thunar as default file manager
