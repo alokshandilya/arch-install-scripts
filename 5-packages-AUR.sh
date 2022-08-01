7bb4ad780f22c9752ccf1ae83f78973e29c35a6a
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-01 16:22:26 +0530

    move packages to pkglist.txt

--- 5-packages-AUR.sh
+++ 5-packages-AUR.sh
@@ -19,21 +19,6 @@ else
   printf "#######################################\n"
 fi
 
-##################################################
-###################### AURs ######################
-##################################################
-## COPY AND RUN ON ANOTHER TERM INSTANCE (TO SAVE TIME) ##
-#o
-#paru -S --needed spotify spotify-adblock-git visual-studio-code-bin ananicy-cpp
-#paru -S --needed android-sdk-platform-tools anydesk-bin caffeine-ng google-earth-pro
-#paru -S --needed mailspring masterpdfeditor-free xdman zoom zramd envycontrol dropbox
-#paru -S --needed nohang-git auto-cpufreq
-#sudo systemctl enable --now zramd.service
-#sudo systemctl enable --now ananicy-cpp.service
-#sudo systemctl enable --now nohang-desktop.service
-#sudo systemctl enable --now systemd-oomd.service
-#sudo systemctl enable --now auto-cpufreq.service
-
 #######################################
 ##### Theme[Orchis], Icons[Qogir] #####
 #######################################
@@ -49,22 +34,33 @@ cd Qogir-icon-theme
 sudo ./install.sh
 cd
 
-#####################################################
-########### VirtualBox and MORE! Packages ###########
-#####################################################
-paru -S --needed virtualbox virtualbox-guest-iso zathura zathura-pdf-poppler gcolor2
-paru -S --needed virtualbox-ext-oracle fish texlive-most starship ttf-twemoji stow pacman-contrib
-paru -S --needed pkgfile dmidecode thunderbird rhythmbox tree
+##################################################
+###################### AURs ######################
+##################################################
+# COPY AND RUN ON ANOTHER TERM INSTANCE (TO SAVE TIME)
+#
+#paru -S --needed spotify spotify-adblock-git visual-studio-code-bin ananicy-cpp
+#paru -S --needed android-sdk-platform-tools anydesk-bin caffeine-ng google-earth-pro
+#paru -S --needed mailspring masterpdfeditor-free xdman zoom zramd envycontrol dropbox
+#paru -S --needed nohang-git auto-cpufreq
+#sudo systemctl enable --now zramd.service
+#sudo systemctl enable --now ananicy-cpp.service
+#sudo systemctl enable --now nohang-desktop.service
+#sudo systemctl enable --now systemd-oomd.service
+#sudo systemctl enable --now auto-cpufreq.service
+
+########################################################
+########### VirtualBox, fish, startship, fnm ###########
+########################################################
+paru -S --needed virtualbox virtualbox-guest-iso pkgfile unzip wget
+paru -S --needed virtualbox-ext-oracle fish starship ttf-twemoji
 sudo pkgfile --update
 chsh -s /usr/bin/fish
-paru -S --needed stylua gparted mpv yt-dlp libreoffice-fresh tumbler pcmanfm ffmpegthumbnailer xarchiver exa fzf zoxide ripgrep neovim python-neovim xclip libgnome-keyring python-keyring
-paru -S --needed wget telegram-desktop redshift flameshot unzip
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 sudo modprobe vboxdrv
 sudo usermod -aG adbusers vboxusers aloks
-
-# Setting thunar as default file manager
-xdg-mime default thunar.desktop inode/directory
+# Setting pcmanfm as default file manager
+xdg-mime default pcmanfm.desktop inode/directory
 
 printf "\n\n"
 printf "############################\n"
