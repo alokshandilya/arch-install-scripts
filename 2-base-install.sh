9cce35d14c5f6c5dab6850f951a1229b93508a40
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-03 21:34:45 +0530

    ufw

--- 2-base-install.sh
+++ 2-base-install.sh
@@ -17,7 +17,14 @@ pacman -S --needed wpa_supplicant mtools dosfstools reflector base-devel avahi x
 pacman -S --needed xdg-utils gvfs gvfs-smb gvfs-mtp nfs-utils inetutils dnsutils bluez bluez-utils cups hplip
 pacman -S --needed alsa-utils pipewire pipewire-alsa pipewire-pulse pavucontrol bash-completion openssh rsync reflector
 pacman -S --needed acpi virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset
-pacman -S --needed firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font android-udev
+pacman -S --needed flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font android-udev
+pacman -S --needed ufw gufw
+
+ufw default deny
+ufw allow from 192.168.0.0/24
+ufw allow Deluge
+ufw limit ssh
+ufw enable
 
 # pacman -S acpi_call-lts nvidia-lts
 # pacman -S acpi_call nvidia
@@ -29,6 +36,7 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
+systemctl enable ufw.service
 systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
@@ -36,7 +44,6 @@ systemctl enable avahi-daemon
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
-systemctl enable firewalld
 systemctl enable acpid
 systemctl enable systemd-resolved.service
 
