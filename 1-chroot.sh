9866d47c7537361cb803c32a2bfa2129e2932c23
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-06 03:13:01 +0530

    add encryption option

--- 1-chroot.sh
+++ 1-chroot.sh
@@ -30,3 +30,40 @@ mount /dev/nvme0n1p1 /mnt/boot/efi
 pacstrap -i /mnt base btrfs-progs linux-zen linux-zen-headers linux-firmware vim intel-ucode git snapper
 genfstab -U /mnt >> /mnt/etc/fstab
 #arch-chroot /mnt
+
+# =====================================================================================================================================
+# =========================== ENCRYPTION ==============================================================================================
+# =====================================================================================================================================
+#
+#------------#-------------#
+# Subvolume  # Mountpoint  #
+#------------#-------------#
+# @          # /           #
+# @home      # /home       #
+# @snapshots # /.snapshots #
+# @var_log   # /var/log    #
+#------------#-------------#
+#
+# mkfs.fat -F32 /dev/nvme0n1p1 -n "EFI"
+# cryptsetup --cipher aes-xts-plain64 --hash sha512 --use-random --verify-passphrase luksFormat /dev/nvme0n1p2
+# cryptsetup luksOpen /dev/nvme0n1p2 cryptroot
+# mkfs.btrfs -f /dev/mapper/cryptroot -L "BTRFS"
+#
+# mount /dev/mapper/cryptroot /mnt
+# btrfs su cr /mnt/@
+# btrfs su cr /mnt/@home
+# btrfs su cr /mnt/@snapshots
+# btrfs su cr /mnt/@var_log
+#
+# umount /mnt
+#
+# mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@ /dev/mapper/cryptroot /mnt
+# mkdir -p /mnt/{home,boot/efi,var/log,.snapshots}
+# mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@home /dev/mapper/cryptroot /mnt/home
+# mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
+# mount -o defaults,noatime,compress=zstd,discard=async,space_cache=v2,autodefrag,subvol=@var_log /dev/mapper/cryptroot /mnt/var/log
+# mount /dev/nvme0n1p1 /mnt/boot/efi
+#
+# pacstrap -i /mnt base btrfs-progs linux-zen linux-zen-headers linux-firmware vim intel-ucode git snapper
+# genfstab -U /mnt >> /mnt/etc/fstab
+# #arch-chroot /mnt
