63d8bc896875306975d0598342e1d5112ff7259b
alokshandilya <alok.shandilya10@gmail.com>
2024-03-19 18:42:49 +0530

    add script after arch install

---
+++ 0-arch-install-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo pacman -S --needed btrfs-progs linux-lts-headers linux-firmware vim intel-ucode git snapper neovim archlinux-keyring stow
+sudo pacman -S --needed ttf-jetbrains-mono-nerd ttf-firacode-nerd ttf-jetbrains-mono
+
+#git config --global core.editor nvim
+#git config --global user.name alokshandilya
+#git config --global user.email alok.shandilya10@gmail.com
+
+#cd
+#git clone https://github.com/alokshandilya/dotfiles.git
+#git clone https://github.com/alokshandilya/nvim.git ~/.config/nvim
+#cd dotfiles
+#mkdir -p ~/.local/share/applications
+#mkdir -p ~/.local/bin/scripts
+#mkdir -p ~/.local/bin/dwmblocks
+#stow .
+
+## add from 2nd script
+
+#####################
+
+
+
