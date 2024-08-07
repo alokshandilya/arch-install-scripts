3c8e3341daa6085e815673ebff0e2a8cbcf48540
alokshandilya <alok.shandilya10@gmail.com>
2024-08-07 21:56:18 +0530

    chore: Update AUR package dependencies, install additional tools, and enable Docker

--- 5-packages-AUR.sh
+++ 5-packages-AUR.sh
@@ -61,13 +61,17 @@ pipx install grip black nose pytest nose pyflakes isort pipenv pyright
 
 paru -S --needed virtualbox-ext-oracle zsh starship ttf-twemoji docker docker-buildx
 sudo ln -sf /usr/share/fontconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/75-twemoji.conf
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 sudo pkgfile --update
-chsh -s /usr/bin/fish
+chsh -s /usr/bin/zsh
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 sudo modprobe vboxdrv
 sudo usermod -aG adbusers,vboxusers,kvm aloks
 # Setting pcmanfm as default file manager
 xdg-mime default pcmanfm.desktop inode/directory
+
+sudo systemctl enable docker.socket
+sudo usermod -aG docker aloks
 
 printf "\n\n"
 printf "############################\n"
